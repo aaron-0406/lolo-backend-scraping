bda84d96c6b9105187fd2e8e84f4f8bf949fdb14
LuisSilvaB <luisarmandoballadares@gmail.com>
2024-10-31 01:11:10 -0500

    chore: change to scan by cron job

--- src/index.ts
+++ src/index.ts
@@ -103,34 +103,34 @@ app.get("/ping", (_req, res) => {
 });
 
 
-  (async () => {
-    console.log("Using manual boot scan 🚀")
-    await service.resetAllCaseFiles()
-    await service.main()
-  }
-  )();
+  // (async () => {
+  //   console.log("Using manual boot scan 🚀")
+  //   await service.resetAllCaseFiles()
+  //   await service.main()
+  // }
+  // )();
 
-  // (async() => await caseFilesService.currencyExchange())();
+  (async() => await caseFilesService.currencyExchange())();
 
-  // cron.schedule('0 6 * * *', async () => {
-  //   await service.resetAllCaseFiles();
-  //   console.log('Cron job iniciado: 6 AM');
-  //   await processCaseFiles();
+  cron.schedule('0 6 * * *', async () => {
+    await service.resetAllCaseFiles();
+    console.log('Cron job iniciado: 6 AM');
+    await processCaseFiles();
 
-  //   async function processCaseFiles() {
-  //     const { notScanedCaseFiles, errorsCounter } = await service.main();
+    async function processCaseFiles() {
+      const { notScanedCaseFiles, errorsCounter } = await service.main();
 
-  //     if (notScanedCaseFiles  || errorsCounter > 4) {
-  //       console.log("Case files with no scan, retrying in 30 minutes.");
+      if (notScanedCaseFiles  || errorsCounter > 4) {
+        console.log("Case files with no scan, retrying in 30 minutes.");
 
-  //       setTimeout(async () => {
-  //         await processCaseFiles();
-  //       }, 30 * 60 * 1000);
-  //     } else {
-  //       console.log("All case files scanned.");
-  //     }
-  //   }
-  // });
+        setTimeout(async () => {
+          await processCaseFiles();
+        }, 30 * 60 * 1000);
+      } else {
+        console.log("All case files scanned.");
+      }
+    }
+  });
 
   console.log("server is running on port", process.env.PORT || 3000);
 });
