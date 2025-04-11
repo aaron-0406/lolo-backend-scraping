4ed21870e00b3b6f79f34d3530750fedf119b484
Aaron Paredes Cabrera <iron0406@gmail.com>
2025-04-11 09:20:04 -0500

    chore: last changes

--- src/index.ts
+++ src/index.ts
@@ -113,7 +113,7 @@ app.get("/ping", (_req, res) => {
 
   // (async() => await caseFilesService.currencyExchange())();
 
-  cron.schedule('11 9 * * *', async () => {
+  cron.schedule('21 9 * * *', async () => {
     await service.resetAllCaseFiles();
     console.log('Cron job iniciado: 6 AM');
     await processCaseFiles();
