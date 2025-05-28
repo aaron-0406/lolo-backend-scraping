78e7b99c46e21febb2efa1f6bc0fada33740e40e
Aaron Paredes Cabrera <iron0406@gmail.com>
2025-05-28 08:17:58 -0500

    chore: last changes

--- src/index.ts
+++ src/index.ts
@@ -132,7 +132,7 @@ app.get("/ping", (_req, res) => {
   }
 
 
-  cron.schedule('48 21 * * *', async () => { // ejecutar cada minuto
+  cron.schedule('30 8 * * *', async () => { // ejecutar cada minuto
       await service.resetAllCaseFiles();
       console.log('cron job iniciado: 11:30 am');
 
